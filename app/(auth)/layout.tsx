2d57e63bfa3a0d3bfb28003a4f21682b6a708cea
utibe edet <utibeabasi.okon@myrecharge.ng>
2024-09-10 00:57:35 -0700

    first commit

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
     <div className="flex flex-col min-h-screen">
       {/* <NavBar /> */}
       <main className="flex-1">{children}</main>
-      <Footer />
+     
     </div>
   );
 }
